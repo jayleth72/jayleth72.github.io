4ce390a36e23e4e8ee15ab246f25575366f3b0c5
Jay Letheby <jay.letheby@gmail.com>
2016-07-05 16:33:28 +1000

    links

--- js/active-menu.js
+++ js/active-menu.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
         $('ul.nav > li').click(function (e) {
-            e.preventDefault();
+            
             $('ul.nav > li').removeClass('active');
             $(this).addClass('active');                
         });            
