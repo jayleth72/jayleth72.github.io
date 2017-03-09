955de8b7dd046d87a4597d93afd7ac42fa5d050a
Jay Letheby <jay.letheby@gmail.com>
2017-03-09 16:30:51 +1000

    nuke and start again

--- js/active-menu.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-        $('ul.nav > li').click(function (e) {
-            
-            $('ul.nav > li').removeClass('active');
-            $(this).addClass('active');                
-        });            
-    });
