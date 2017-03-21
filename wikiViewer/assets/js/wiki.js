a8ac69f9c6ec75477173c1a5a27ba3b100ed7ead
Jason Letheby <jay.letheby@gmail.com>
2017-03-21 17:32:21 +1000

    added js file

---
+++ wikiViewer/assets/js/wiki.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  // run search when search button is called
+  $('#search').click(function(){
+    var searchTerm = $('#searchTerm').val();
+    // API url with the search Term
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+
+    $.ajax({
+      type:"GET",
+      url:url,
+      async:true,
+      success:function(data){
+        console.log(data);
+      },
+      error: function(errorMessage){
+        alert("Error");
+      }
+    });
+  });
+});
