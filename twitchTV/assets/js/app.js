7a26cbe573639946dfc861d09b563e7939397e3f
Jason Letheby <jay.letheby@gmail.com>
2017-03-31 12:09:13 +1000

    status working for twitch tv

--- twitchTV/assets/js/app.js
+++ twitchTV/assets/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   // free code camp stream and status API CALL
-  var url="https://wind-bow.gomix.me/twitch-api/freecodecamp"
+  var url="https://api.twitch.tv/kraken/streams/freecodecamp";
   $.ajax({
     type:"GET",
     url:url,
@@ -8,7 +8,12 @@ $(document).ready(function(){
       'Client-ID': 'j2hby0a1godecj1l4ak96qs1tp3y3y'
     },
     success: function(data1){
-      console.log(data1);
+      if(data1.stream===null){
+        $("#fcc-status").html("Free Code Camp is currently offline");
+      }
+      else{
+          $("#fcc-status").html("Free Code Camp is Online");
+      }
     }
   });
 });
